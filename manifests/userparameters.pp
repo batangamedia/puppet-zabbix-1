fd662c8bb74a071b1b1adc1fd7e36884d281471c
Fabio Gallese <fabio@fabiogallese.com>
2016-10-18 20:41:09 +0200

    Removed notify

--- manifests/userparameters.pp
+++ manifests/userparameters.pp
@@ -103,7 +103,6 @@ define zabbix::userparameters (
   # which needs to be loaded for this host. When exported resources is
   # used/enabled, we do this automatically.
   if $template != '' {
-    notify { "Estoy exportando el recurso necesario para agregar el template $template": }
     zabbix::resources::userparameters { "${::hostname}_${name}":
       hostname => $::fqdn,
       template => $template,
