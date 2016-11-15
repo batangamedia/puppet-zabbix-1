0ee1fc9aebc9a6d4c10198d66515eb8b09278d23
Fabio Gallese <fgallese@gmail.com>
2016-11-15 15:21:16 +0100

    Removed un-needed notify

--- manifests/hostgroups.pp
+++ manifests/hostgroups.pp
@@ -3,7 +3,6 @@ define zabbix::hostgroups (
   $hostgroup  = '',
 ) {
   if $hostgroup != '' {
-    notify { "Estoy asignando el hostgroup $hostgroup al servidor en zabbix": }
     zabbix::resources::hostgroups { "${::hostname}_${name}":
       hostgroup => $hostgroup,
       hostname => $hostname,
